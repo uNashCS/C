143e6d935d8daf2d7149a36b7b39ff11fd00d5c6
uNashCS <80352244+uNashCS@users.noreply.github.com>
2021-06-23 15:02:51 -0500

    Mod basic

--- 01-introduccion.c
+++ 01-introduccion.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include <stdio.h>                                  //Librería <stdio.h> Información de entrada y salida
 /*
-Inicia la ejecución del programa
+Inicia la ejecución del programa MAIN
 */
-int main()
+int main()                                          // Función principal reconidas por () comienza todo programa por main
 {
-    printf("Hola mundo\n");
-    return 0;
+    printf("Hola mundo\n");                         // función printf -> \n caracter de escape
+                                                    //          \n nueva línea
+                                                    //          
+    return 0;                                       // El progrma se terminó con éxito
 }
 /*
 Fin de la función
--- 02-suma.c
+++ 02-suma.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
-
+/* Incio de función MAIN */
 int main()
 {
-    int num1;
+    int num1;                                   // Declaración de variables
     int num2;
     int suma;
     int resta;
     int multiplicacion;
-    printf("Ingrese primer valor:\n");
-    scanf("%d",&num1);
+    printf("Ingrese primer valor:\n");          // Imprime diálogo
+    scanf("%d",&num1);                          // Lee números de entrada de teclado y el %d indica que es un número el que va a recibir.       scanf("%d",DATO-INT)
+                                                // & operador de dirección, la ruta o la direccion donde va a ser almacenada
     printf("Ingrese segundo valor:\n");
     scanf("%d",&num2);
-    suma=num1+num2;
+    suma=num1+num2;                             // Realiza las operaciones de suma
     resta=num1-num2;
     multiplicacion=num1*num2;
-    printf("La suma es: %d\n", suma);
+    printf("La suma es: %d\n", suma);           // Indica %d que es un INT el que va a imprimir printf("%d", DATON-INT)
     printf("La resta es: %d\n", resta);
     printf("La multiplicación es: %d\n", multiplicacion);
-    return 0;
+    return 0;                                   // Finalizó correctamente
 }
+/* Fin de función MAIN */
